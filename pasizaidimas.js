75b42bb5a5b6cb1e54781bf1a8381158cfb12958
Tomas.K <tomas.konopackis@gmail.com>
2024-05-09 17:40:42 +0300

    pasitreniravimui

--- pasizaidimas.js
+++ pasizaidimas.js
@@ -1,15 +1,24 @@
 let myName = "Tomas";
 let myAge = 34;
 
-console.log (myName);
-console.log (myAge);
+console.log(myName);
+console.log(myAge);
 
-console.log (typeof myName);
-console.log (typeof myAge);
+console.log(typeof myName);
+console.log(typeof myAge);
 
 myName = "Jonas";
 
-console.log (myName);
-console.log (typeof myName);
-console.log ("Tomas");
+console.log(myName);
+console.log(typeof myName);
+console.log("Tomas");
 
+const zeroFuel = (distanceToPump, mpg, fuelLeft);
+function answer(zeroFuel) {
+    if (distanceToPump - (mpg * fuelLeft) >= 0) {
+    }
+    return answer;
+}
+
+
+console.log(zeroFuel(50, 25, 1));

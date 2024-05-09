3e3b8f3e66d9fd970f5bf1b8c7523eb75c038cbe
Tomas.K <tomas.konopackis@gmail.com>
2024-05-09 17:43:18 +0300

    papildyta

--- js/funkcijos/function.js
+++ js/funkcijos/function.js
@@ -8,18 +8,22 @@ input => Function() => Output
 
 function funkcijosPavadinimas() {
 }
+//1) Input (parametru) validavimas
+//2) vykdome logika
+//3) Gauto rezultato validavimas
+//4) Graziname rezultata
 
-const a  = funkcijosPavadinimas();
+const a = funkcijosPavadinimas();
 
-console.log (a);
+console.log(a);
 
-function empty () {
+function empty() {
     return undefined;
 }
 
-console.log (empty());
+console.log(empty());
 
-function giveMeFive (){
+function giveMeFive() {
     return 5;
 }
 console.log(giveMeFive());
@@ -32,13 +36,13 @@ surname: Jonaitis
 Output:
 Laba diena, as esu Jonas Jonaitis!
 */
-function intro (name, lastname, age){
+function intro(name, lastname, age) {
     return "Laba diena, as esu " + name + " " + lastname + " mano amzius yra " + age + "!";
 }
 
-console.log (intro("Jonas", "Jonaitis", 30));
-console.log (intro("Maryte", "Marytaite", 20));
-console.log (intro("Ona", "Onaityte", 25));
+console.log(intro("Jonas", "Jonaitis", 30));
+console.log(intro("Maryte", "Marytaite", 20));
+console.log(intro("Ona", "Onaityte", 25));
 
 console.clear();
 
@@ -53,37 +57,37 @@ Output:
 35
 */
 
-function multiply(firstNumber, secondNumber){
+function multiply(firstNumber, secondNumber) {
     return firstNumber * secondNumber;
 }
 
-console.log (multiply(10, 20));
-console.log (multiply(5, 8));
-console.log (multiply(-6, 15));
-console.log ("-------");
+console.log(multiply(10, 20));
+console.log(multiply(5, 8));
+console.log(multiply(-6, 15));
+console.log("-------");
 
 
 // Mano vardas yra Jonas, man 99 metai ir as megstu cepelinus
 // Mano vardas yra Maryte, man 88 metai ir as megstu balionus
 
-function iLike (name, age, like){
+function iLike(name, age, like) {
     return "Mano vardas yra " + name + " man " + age + " metai ir as megstu " + like + ".";
 }
 
-console.log (iLike("Jonas", 99, "cepelinus"));
-console.log (iLike("Maryte", 88, "balionus"));
+console.log(iLike("Jonas", 99, "cepelinus"));
+console.log(iLike("Maryte", 88, "balionus"));
 
 console.log("---------------------");
 
-function price (value1){
+function price(value1) {
     const pvm = 21;
-    const priceForSale = value1 * (1 + pvm/100);
+    const priceForSale = value1 * (1 + pvm / 100);
     const pvmPrice = value1 * 0.21;
-    return "Price: " + value1 + "Eur\n" +  "PVM: " +
-    pvmPrice + "Eur\n" + "Total: " + priceForSale + "Eur\n" + "Have a nice day!";
+    return "Price: " + value1 + "Eur\n" + "PVM: " +
+        pvmPrice + "Eur\n" + "Total: " + priceForSale + "Eur\n" + "Have a nice day!";
 
- }
-console.log (price(5));
+}
+console.log(price(5));
 
 
 

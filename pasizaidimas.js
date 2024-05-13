8ee4219b300f5dd4573783ff81684e2cb2348f62
Tomas.K <tomas.konopackis@gmail.com>
2024-05-13 11:57:10 +0300

    treniruojames

--- pasizaidimas.js
+++ pasizaidimas.js
@@ -32,3 +32,114 @@ function answer(distanceToPump, mpg, fuelLeft) {
 
 
 console.log(answer(50, 10, 3));
+
+
+
+console.clear();
+
+
+console.log("------------Zaidimukas--------------");
+
+
+function check(a, x) {
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] === x) {
+            return true;
+        }
+    }
+    return false;
+}
+
+console.log(check([66, 101], 66));
+console.log(check([101, 45, 75, 105, 99, 107], 107));
+console.log(check(['t', 'e', 's', 't'], 'e'));
+console.log(check(['what', 'a', 'great', 'kata'], 'kat'));
+console.log(check(["jonas", 65, 100], 65));
+
+
+function positiveSum(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            sum += arr[i];
+        }
+    }
+    return sum;
+}
+
+
+console.log(positiveSum([1, 2, 3, 4, 5]));
+console.log(positiveSum([1, -2, 3, 4, 5]));
+console.log(positiveSum([-1, -2, -3, -4, -5]));
+console.log(positiveSum([-1, 2, 3, 4, -5]));
+
+console.clear();
+
+
+function arrayMadness(a, b) {
+    sumA = 0;
+    sumB = 0;
+    for (let i = 0; i < a.length; i++) {
+        sumA += (a[i]) ** 2;
+    }
+    for (let i = 0; i < b.length; i++) {
+        sumB += (b[i]) ** 3;
+    }
+    if (sumA > sumB) {
+        return true;
+    }
+    return false;
+}
+
+console.log(arrayMadness([4, 5, 6], [1, 2, 3]));
+console.log(arrayMadness([5, 6, 7], [4, 5, 6]));
+console.log(arrayMadness([4, 5, 6], [3, 4, 5]));
+console.log(arrayMadness([3, 4, 5], [2, 3, 4]));
+console.log(arrayMadness([2, 3, 4], [1, 2, 3]));
+console.log(arrayMadness([1, 2, 3], [0, 1, 2]));
+console.log(arrayMadness([5, 3, 2, 4, 1], [15]));
+console.log(arrayMadness([2, 5, 3, 4, 1], [3, 3, 3, 3, 3]));
+console.log(arrayMadness([1, 3, 5, 2, 4], [2, 2, 2, 2, 2, 2, 2, 1]));
+console.log(arrayMadness([1, 2, 3, 4, 5], [2, 2, 2, 2, 2, 2, 1, 1, 1]));
+console.log(arrayMadness([2, 4, 6, 8, 10, 12, 14], [1, 3, 5, 7, 9, 11, 13]));
+
+
+console.clear();
+
+
+function strCount(str, letter) {
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === letter) {
+            count++;
+        }
+    }
+    return count;
+}
+
+console.log(strCount('Hello', 'o'));
+console.log(strCount('Hello', 'l'));
+console.log(strCount('', 'z'));
+
+console.clear();
+
+
+function differenceInAges(ages) {
+    let youngest = ages[0];
+    let oldest = ages[0];
+    for (let i = 0; i < ages.length; i++) {
+        if (ages[i] < youngest) {
+            youngest = ages[i]
+        }
+    }
+    for (let i = 0; i < ages.length; i++) {
+        if (ages[i] > oldest) {
+            oldest = ages[i];
+        }
+    }
+    return [youngest, oldest, oldest - youngest];
+}
+
+console.log(differenceInAges([82, 15, 6, 38, 35]), [6, 82, 76]);
+console.log(differenceInAges([57, 99, 14, 32]), [14, 99, 85]);
+

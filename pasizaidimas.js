84df12b52e2fd3df21474729cd4a768baddf965a
Tomas.K <tomas.konopackis@gmail.com>
2024-05-14 15:12:51 +0300

    codewar sprendimas su raidziu pakeitimu tekste

--- pasizaidimas.js
+++ pasizaidimas.js
@@ -155,3 +155,168 @@ function past(h, m, s) {
 }
 
 console.log(past(0, 1, 1));
+
+function summation(num) {
+    let sum = 0;
+    for (let i = 0; i <= num; i++) {
+        sum += i;
+    }
+    return sum;
+}
+
+console.log(summation(1));
+console.log(summation(2));
+console.log(summation(8));
+console.log(summation(5));
+
+function tripleTrouble(one, two, three) {
+    let sumText = "";
+    if (one.length === two.length === three.length) {
+        return "input length not equal";
+    }
+    for (let i = 0; i < one.length; i++) {
+        sumText += (one[i] + two[i] + three[i]);
+    }
+    return sumText;
+}
+
+
+console.log(tripleTrouble("aaa", "bbb", "ccc"));
+console.log(tripleTrouble("aaaaaa", "bbbbbb", "cccccc"));
+console.log(tripleTrouble("burn", "reds", "roll"));
+console.log(tripleTrouble("Sea", "urn", "pms"));
+console.log(tripleTrouble("LLh", "euo", "xtr"));
+
+console.clear();
+
+
+function liters(time) {
+    return parseInt(time * 0.5);
+}
+
+
+console.log(liters(2));
+console.log(liters(1.4));
+console.log(liters(12.3));
+console.log(liters(0.82));
+console.log(liters(11.8));
+console.log(liters(1787));
+console.log(liters(0));
+
+
+console.clear();
+
+function areaOrPerimeter(l, w) {
+    let square = l * w;
+    let perimetter = (l + w) * 2;
+    if (l === w) {
+        return square;
+    }
+    return perimetter;
+}
+
+
+console.log(areaOrPerimeter(3, 3));
+console.log(areaOrPerimeter(6, 10));
+
+console.clear();
+
+
+function guessBlue(blueStart, redStart, bluePulled, redPulled) {
+    let blueLeft = blueStart - bluePulled;
+    let redLeft = redStart - redPulled;
+    let chances = blueLeft / (blueLeft + redLeft);
+    if (bluePulled > blueStart || redPulled > redStart) {
+        return "Wrong input";
+    }
+    return parseFloat(chances);
+}
+
+
+console.log(guessBlue(5, 5, 2, 3));
+console.log(guessBlue(5, 7, 4, 3));
+console.log(guessBlue(12, 18, 4, 6));
+
+console.clear();
+
+console.log("" + parseFloat);
+console.log("" + toString(parseFloat));
+
+console.clear();
+
+
+function correctPolishLetters(string) {
+    let result = '';
+
+    for (let i = 0; i < string.length; i++) {
+        const letter = string[i];
+        if (letter === 'ą') {
+            result += 'a';
+        } else if (letter === 'ć') {
+            result += 'c';
+        } else if (letter === 'ę') {
+            result += 'e';
+        } else if (letter === 'ł') {
+            result += 'l';
+        } else if (letter === 'ń') {
+            result += 'n';
+        } else if (letter === 'ó') {
+            result += 'o';
+        } else if (letter === 'ś') {
+            result += 's';
+        } else if (letter === 'ź') {
+            result += 'z';
+        } else if (letter === 'ż') {
+            result += 'z';
+        }
+        else {
+            result += letter;
+        }
+    }
+    return result;
+}
+
+
+
+console.log(correctPolishLetters("Wałęsa"));
+console.log(correctPolishLetters("Jędrzęj Błądziński"));
+console.log(correctPolishLetters("Lech Wałęsa"));
+console.log(correctPolishLetters("Maria Skłodowska-Curie"));
+// ą -> a,
+// ć -> c,
+// ę -> e,
+// ł -> l,
+// ń -> n,
+// ó -> o,
+// ś -> s,
+// ź -> z,
+// ż -> z
+
+
+// function correctPolishLetters1(string) {
+//     const polisAbc = "ąćęłńóśźż";
+//     const normalAbc = "acelnoszz";
+//     for (let i = 0; i < string.length; i++) {
+//         const letter = string[i];
+
+//         //kur sedi ę polishAbc index 2
+//         //kur sedi normalAbc e index 2
+//         let index = -1;
+//         for (let j = 0; j < polisAbc.length; j++) {
+//             const polishletter = polisAbc[j];
+//             if (polishletter === letter) {
+//                 result += normalAbc[j];
+//                 index = j;
+//             }
+//         }
+//         if (index === -1) {
+//             result += letter;
+//         }
+//     }
+//     return result;
+// }
+
+// console.log(correctPolishLetters1("Wałęsa"));
+// console.log(correctPolishLetters1("Jędrzęj Błądziński"));
+// console.log(correctPolishLetters1("Lech Wałęsa"));
+// console.log(correctPolishLetters1("Maria Skłodowska-Curie"));

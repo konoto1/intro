00c0fdcd84b8918921d56294b0c1f3a6286ff5a4
Tomas.K <tomas.konopackis@gmail.com>
2024-05-16 16:43:42 +0300

    trenirruojames

--- pasizaidimas.js
+++ pasizaidimas.js
@@ -740,3 +740,17 @@ console.log(findNeedle(['3', '123124234', undefined, 'needle', 'world', 'hay', 2
 console.log(findNeedle(['283497238987234', 'a dog', 'a cat', 'some random needle junk', 'a piece of hay', 'needle', 'something somebody lost a while ago']));
 console.log(findNeedle([1, 2, 3, 4, 5, 6, 7, 8, 8, 7, 5, 4, 3, 4, 5, 6, 67, 5, 5, 3, 3, 4, 2, 34, 234, 23, 4, 234, 324, 324, 'needle', 1, 2, 3, 4, 5, 5, 6, 5, 4, 32, 3, 45, 54]));
 
+console.clear();
+console.log('-----Sentence Smash----');
+
+
+function smash(words) {
+    return words.toString().replaceAll(',', ' ');
+}
+
+
+console.log(smash(["hello", "world"]));                                        // "hello world");
+console.log(smash(["hello", "amazing", "world"]));                             // "hello amazing world");
+console.log(smash(["this", "is", "a", "really", "long", "sentence"]));         // "this is a really long sentence");
+
+

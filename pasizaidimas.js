7c0702ecea594be5498b34ff8fbb1a35105bd531
Tomas.K <tomas.konopackis@gmail.com>
2024-05-16 14:46:10 +0300

    papildytas

--- pasizaidimas.js
+++ pasizaidimas.js
@@ -669,3 +669,74 @@ console.log(position("e"));     //"Position of alphabet: 5");
 
 console.clear();
 console.log('----------Find the position!------')
+
+
+
+const array1 = ['Tomas', ['Algis', 'Ona'], 'Jonas', 'Petras'];
+const iterator1 = array1[Symbol.iterator]();
+
+for (const value of iterator1) {
+    console.log(value);
+}
+
+
+const arrayA = ['Tomas', 'b', 'd'];
+const arrayB = ['c', 'e', 'f'];
+const arrayC = arrayA.concat(arrayB);
+
+console.log(arrayC);
+
+
+const arrayZ = ['a', 'b', 'c', 'd', 'e'];
+
+console.log(arrayZ.copyWithin(0, 1, 7));
+
+
+console.clear();
+console.log('-----------Multiple of index----------------');
+
+function multipleOfIndex(array) {
+    let newArray = [];
+    let values = 0;
+    if (array[0] === 0) {
+        newArray = [0];
+    }
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] % i === 0) {
+            values = array[i];
+            newArray.push(values);
+        }
+    }
+    return newArray;
+}
+
+
+
+console.log(multipleOfIndex([22, -6, 32, 82, 9, 25]));                                                                     // [-6, 32, 25]],
+console.log(multipleOfIndex([68, -1, 1, -7, 10, 10]));                                                                     // [-1, 10]],
+console.log(multipleOfIndex([11, -11]));                                                                                  // [-11]))
+console.log(multipleOfIndex([-56, -85, 72, -26, -14, 76, -27, 72, 35, -21, -67, 87, 0, 21, 59, 27, -92, 68]));            // [-85, 72, 0, 68]],
+console.log(multipleOfIndex([28, 38, -44, -99, -13, -54, 77, -51]));                                                      // [38, -44, -99]],
+console.log(multipleOfIndex([-1, -49, -1, 67, 8, -60, 39, 35]));                                                          // [-49, 8, -60, 35]],
+console.log(multipleOfIndex([0, 2, 3, 6, 9]));                                                                            // [0, 2, 6]]
+
+
+const words = [68, -1, 1, -7, 10, 10];
+
+const result = words.filter((num) => num % words.indexOf(num) === 0);
+
+console.log(0 / 0);
+
+
+console.clear();
+console.log('--------A Needle in the Haystack-------');
+
+function findNeedle(haystack) {
+    return 'found the needle at position ' + haystack.indexOf('needle');
+}
+
+
+console.log(findNeedle(['3', '123124234', undefined, 'needle', 'world', 'hay', 2, '3', true, false]));
+console.log(findNeedle(['283497238987234', 'a dog', 'a cat', 'some random needle junk', 'a piece of hay', 'needle', 'something somebody lost a while ago']));
+console.log(findNeedle([1, 2, 3, 4, 5, 6, 7, 8, 8, 7, 5, 4, 3, 4, 5, 6, 67, 5, 5, 3, 3, 4, 2, 34, 234, 23, 4, 234, 324, 324, 'needle', 1, 2, 3, 4, 5, 5, 6, 5, 4, 32, 3, 45, 54]));
+

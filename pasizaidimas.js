38c7c7b7c75178dcc4155fed6b7c8db409f0dede
Tomas.K <tomas.konopackis@gmail.com>
2024-06-07 09:57:22 +0300

    treniruojames

--- pasizaidimas.js
+++ pasizaidimas.js
@@ -2064,3 +2064,50 @@ console.log(sorter(['Algebra', 'history', 'Geometry', 'english']));
 console.log(sorter(['Alg#bra', '$istory', 'Geom^try', '**english']));
 // ['$istory', '**english', 'Alg#bra', 'Geom^try']);
 
+
+console.clear();
+console.log('------Largest 5 digit number in a series-----');
+
+function solution(digits) {
+    let arr = [];
+    for (let i = 0; i < digits.length; i++) {
+        arr.push(digits.slice(i, i + 5));
+        arr.push(digits.slice(i + 5, i + 10));
+    }
+    let big = parseInt(arr[0]);
+    for (let i = 0; i < arr.length; i++) {
+        if (big < parseInt(arr[i])) {
+            big = parseInt(arr[i]);
+        }
+    }
+    return big;
+}
+
+
+
+
+
+// console.log(solution("731671761531330624919225196742657474235534919493496983520368542506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753123457977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257540920752963450"));
+console.log(solution('6248909014293157374325734426843353037'));
+
+console.clear();
+console.log('--------------My Language Skills--------------');
+
+function myLanguages(results) {
+    const arr = Object.getOwnPropertyNames(results);
+    let newArr = [];
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (results[arr[i]] >= 60) {
+            newArr.push({ name: arr[i], score: results[arr[i]] });
+        }
+    }
+    newArr.sort((a, b) => b.score - a.score);
+    for (let i = 0; i < newArr.length; i++) {
+        result.push(newArr[i].name);
+    }
+    return result;
+}
+
+console.log(myLanguages({ "Java": 10, "Ruby": 80, "Python": 65 }));
+console.log(myLanguages({ "Greek": 71, "Hindi": 60, "Dutch": 93 }));
